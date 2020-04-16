f06d1082e5dfe18215c909c7183b5161eda1a406
Larry Xu <larry@larryxu.com>
2020-04-15 23:29:00 -0700

    Add new questions

--- js/cards.js
+++ js/cards.js
@@ -1,13 +1,93 @@
 var CARDS = [
   {
-    level: 1,
-    question: "What types of books would we find on your bookshelf? Which book has most influenced you?",
-    source: "Kristine S. and Willie H."
+    level: 2,
+    question: "What is something someone wouldn't know about you when they first meet you?",
+    source: "Arpitha H."
+  },
+  {
+    level: 3,
+    question: "If you could change anything about the way you were raised, what would it be?",
+    source: "36 Questions"
+  },
+  {
+    level: 2,
+    question: "What is a story about you that you wish people knew?",
+    source: "Patrick X."
+  },
+  {
+    level: 3,
+    question: "What moment would you have treasured more, had you known it was going to be the last time?",
+    source: "Reddit"
+  },
+  {
+    level: 2,
+    question: "If you could change one thing about yourself, what would it be? Would you actually change it?",
+    source: "Lauren C."
+  },
+  {
+    level: 2,
+    question: "What advice would you give your younger self? Do you think your younger self would have believed you?",
+    source: "Caroline D."
+  },
+  {
+    level: 3,
+    question: "What is a defining moment in your life that changed the way you viewed the world?",
+    source: "Patrick X."
   },
   {
     level: 1,
-    question: "Show us a photo that means a lot to you and tell us the story behind it.",
+    question: "Show us a photo that means a lot to you and tell us the story behind it",
     source: "Anonymous"
+  },
+  {
+    level: 1,
+    question: "What types of books would we find on your bookshelf? Which book (or podcast) has most influenced you?",
+    source: "Kristen S. & Willie H."
+  },
+  {
+    level: 1,
+    question: "What is the most interesting fact about the place you grew up?",
+    source: "Patrick X."
+  },
+  {
+    level: 1,
+    question: "What is something you have always wanted to learn, but havent yet?",
+    source: "Chelsea W."
+  },
+  {
+    level: 2,
+    question: "What is something you are currently being challenged by?",
+    source: "Monica M."
+  },
+  {
+    level: 2,
+    question: "Describe a ten out of ten day for you. Have you had one? If so, what happened?",
+    source: "Kathy D."
+  },
+  {
+    level: 2,
+    question: "What are you thankful for at this very moment?",
+    source: "Kalina S."
+  },
+  {
+    level: 3,
+    question: "What does a good life mean to you?",
+    source: "Elli B."
+  },
+  {
+    level: 2,
+    question: "If at the end of the year, you had accomplished one thing, what would make the biggest difference to your happiness?",
+    source: "Gretchen R."
+  },
+  {
+    level: 2,
+    question: "What is the most meaningful trip you have been on? What made this trip unique?",
+    source: "Patrick X."
+  },
+  {
+    level: 1,
+    question: "What could you talk about for hours?",
+    source: "Olivia N."
   },
   {
     level: 1,
@@ -15,14 +95,39 @@ var CARDS = [
     source: "Carly M."
   },
   {
+    level: 3,
+    question: "What do you want to do before you die?",
+    source: "The Buried Life"
+  },
+  {
+    level: 2,
+    question: "Who is the most influential person in your life?",
+    source: "Elli B."
+  },
+  {
+    level: 3,
+    question: "If we knew you well, what would we know you were grateful for? What if we knew you really well?",
+    source: "May L."
+  },
+  {
+    level: 3,
+    question: "When you think about your life, what do you find most astonishing and why?",
+    source: "Xavier I."
+  },
+  {
     level: 1,
-    question: "What movie, TV show or podcast would you recommend to someone to get a feel for your personality?",
+    question: "What movie or TV show would you recommend to someone to get a feel for your personality?",
     source: "Laura H."
   },
   {
     level: 2,
-    question: "Describe a perfect day for you. Have you had one? If so, what happened?",
-    source: "Kathy D."
+    question: "What is something beautiful you've seen or felt recently?",
+    source: "Lauren C."
+  },
+  {
+    level: 3,
+    question: "When was the last time you cried?",
+    source: "Miranda L."
   },
   {
     level: 2,
@@ -35,28 +140,23 @@ var CARDS = [
     source: "Patrick X."
   },
   {
-    level: 2,
-    question: "What advice would you give your younger self? Do you think your younger self would have believed you?",
-    source: "Caroline D."
-  },
-  {
     level: 3,
     question: "If you could go back and rewatch one scene in your life without changing it, what would it be?",
     source: "Jess T."
   },
   {
-    level: 3,
-    question: "What is something beautiful you've seen or felt recently?",
-    source: "Lauren C."
+    level: 1,
+    question: "What is the best meal you have ever had?",
+    source: "Kameron F."
   },
   {
-    level: 3,
-    question: "If you could change anything about the way you were raised, what would it be?",
+    level: 2,
+    question: "What is your most memorable childhood memory? How has it impacted you?",
+    source: "Arpitha H."
+  },
+  {
+    level: 2,
+    question: "What is your most treasured or most terrible memory?",
     source: "36 Questions"
-  },
-  {
-    level: 3,
-    question: "What is a defining moment in your life that changed the way you viewed the world?",
-    source: "Patrick X."
   }
 ];
